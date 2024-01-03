78edec2d38960fd60da4acce3083c2d2f4304d16
sojinsamuel <sojinsamuel2001@gmail.com>
2024-01-03 19:44:29 +0530

    change return value to a number

--- src/handler.js
+++ src/handler.js
@@ -3,14 +3,5 @@
 module.exports.generateRandomNumber = (event) => {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   console.log(`Random number generated: ${randomNumber}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Random number generated: ${randomNumber}`,
-      },
-      null,
-      2
-    ),
-  };
+  return randomNumber;
 };
